7d57932bff2cb5104a2a2900f3efadfc108dfbd6
Harry Pidcock <harry.pidcock@canonical.com>
2025-05-08 15:45:43 +1000

    feat: add compat layer for gc

---
+++ compat.go
@@ -0,0 +1,42 @@
+package check
+
+import (
+	"testing"
+
+	"github.com/juju/tc"
+)
+
+type C = tc.C
+type Checker = tc.Checker
+type CheckerInfo = tc.CheckerInfo
+type CommentInterface = tc.CommentInterface
+
+var (
+	IsNil        Checker = tc.IsNil
+	NotNil       Checker = tc.NotNil
+	Equals       Checker = tc.Equals
+	DeepEquals   Checker = deepEquals()
+	HasLen       Checker = tc.HasLen
+	ErrorMatches Checker = tc.ErrorMatches
+	Matches      Checker = tc.Matches
+	Panics       Checker = tc.Panics
+	PanicMatches Checker = tc.PanicMatches
+	FitsTypeOf   Checker = tc.FitsTypeOf
+	Implements   Checker = tc.Implements
+)
+
+func TestingT(t *testing.T) {
+	tc.TestingT(t)
+}
+
+func Suite(suite any) any {
+	return tc.Suite(suite)
+}
+
+func Not(checker Checker) Checker {
+	return tc.Not(checker)
+}
+
+func Commentf(format string, args ...any) CommentInterface {
+	return tc.Commentf(format, args)
+}
---
+++ go.mod
@@ -0,0 +1,14 @@
+module gopkg.in/check.v1
+
+go 1.24.2
+
+require (
+	github.com/juju/tc v0.0.0-20250507150813-1d13c1fc4d6c
+	github.com/kr/pretty v0.3.1
+)
+
+require (
+	github.com/kr/text v0.2.0 // indirect
+	github.com/rogpeppe/go-internal v1.14.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
